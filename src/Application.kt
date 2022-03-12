20e2a437d1c4969f33f6b18195eaf71e449e17f2
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-03-12 23:26:37 +0900

    add verifier

--- src/Application.kt
+++ src/Application.kt
@@ -1,15 +1,54 @@
 package com.example
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
 import io.ktor.auth.Authentication
+import io.ktor.auth.jwt.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.util.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(Authentication) {
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
 
+    install(Authentication) {
+        jwt("auth_jwt") {
+            realm = myRealm
+
+            val verifier = JWT
+                .require(Algorithm.HMAC256(secret))
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .build()
+            verifier(verifier)
+        }
+    }
+
+    routing {
+        post("/login") {
+            val user = call.receive<User>()
+
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.userName)
+                .withExpiresAt(Date(System.currentTimeMillis()+ 60000))
+            call.respond(hashMapOf("token" to token))
+        }
     }
 }
+
+data class User(
+    val userName: String,
+    val password: String
+)
 
