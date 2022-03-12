f7ef0cd8940aa9ce08feaeb8d137b1a634d85ed6
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-03-12 23:07:25 +0900

    ktor version 1.6.4

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,15 @@
 package com.example
 
-import io.ktor.server.application.*
+import io.ktor.application.*
+import io.ktor.auth.Authentication
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
+@JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+
+    }
 }
 
